535f5218c25c38f88db3a54aadad1b12ad10e27c
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-23 14:45:56 +0530

    Input two number and print their sum

---
+++ Main_2.java
@@ -0,0 +1,10 @@
+// Take a user's name as input and print "Hello, [name]!"
+ 
+ import java.util.*;
+ public class Main_2{
+   public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println("Hello"  + " " + name);
+   }
+ }
