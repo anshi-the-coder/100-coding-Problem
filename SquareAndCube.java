244de42ac1472eda036df6290ba3dc8a1db7418e
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-23 15:10:24 +0530

    Input a number and print its square and cube

---
+++ SquareAndCube.java
@@ -0,0 +1,12 @@
+//Input a number and print its square and cube.
+import java.util.*;
+public class SquareAndCube{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int square = a*a;
+    int cube = a*a*a;
+    System.out.println("square is : "+ square);
+    System.out.println("cube is : "+ cube );
+  }
+}
