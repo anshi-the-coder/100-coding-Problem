1259f5008d5c40a5ba9811dd2aa5680862b9bb72
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-23 14:48:53 +0530

    Input two numbers and print their difference

---
+++ Difference.java
@@ -0,0 +1,12 @@
+// Input two numbers and print their difference.
+ 
+ import java.util.*;
+ public class Difference{
+   public static void main(String args[]){
+   Scanner sc = new Scanner (System.in);
+   int a = sc.nextInt();
+   int b = sc.nextInt();
+   int diff = a-b;
+   System.out.println(diff);
+   }
+ }
